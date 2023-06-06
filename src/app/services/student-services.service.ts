bf882442eeb2569518d323a264b3baebf8b41292
Salmanulfaris-M <fsalmanul13@gmail.com>
2023-06-06 14:09:22 +0530

    service and interceptor setup

---
+++ src/app/services/student-services.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { CookieService } from 'ngx-cookie-service';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentServicesService {
+
+  constructor(private http: HttpClient, private cookieService: CookieService) { }
+
+  //check user
+  checkUserExist(userData: any): Observable<any> {
+    return this.http.post('checkstudent', userData, httpOptions)
+  }
+
+  //insert user
+  insertUser(userData: any): Observable<any> {
+    return this.http.post('register', userData, httpOptions)
+  }
+
+  login(userData: any): Observable<any> {
+    return this.http.post('login', userData, httpOptions)
+  }
+
+  //JWT Token taken from browser cokkiestorage
+  getToken(token: string) {
+    if (token == "admin") {
+      return this.cookieService.get('adminjwt')
+    } else if ("intructor") {
+      return this.cookieService.get('instructorjwt')
+    } else {
+      return this.cookieService.get('studentjwt')
+    }
+  }
+
+  studentLog() {
+    return !!this.cookieService.get('studentjwt')
+  }
+
+}
---
+++ src/app/token.interceptor.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { StudentServicesService } from "./services/student-services.service";
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor(private studentService: StudentServicesService) { }
+  token!: string;
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler) {
+    const isAdminRequest = request.url.includes("admin");
+    const isTeacherRequest = request.url.includes("teacher");
+
+    if (isAdminRequest) {
+      this.token = "admin";
+    } else if (isTeacherRequest) {
+      this.token = "teacher";
+    } else {
+      this.token = "student";
+    }
+
+    let authService = this.studentService.getToken(this.token);
+    if (authService) {
+      const headers = {
+        Authorization: 'Bearer' + authService
+      };
+      let newRequest = request.clone({
+        url: environment.backendUrl + request.url,
+        setHeaders: headers
+      })
+      return next.handle(newRequest);
+    } else {
+      let newRequest = request.clone({
+        url: environment.backendUrl + request.url
+      })
+      return next.handle(newRequest);
+    }
+  }
+}
