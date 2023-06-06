9990885d59b3d5e8a7de78292b50afce836db4b3
Salmanulfaris-M <fsalmanul13@gmail.com>
2023-06-06 14:39:17 +0530

    student module setup

--- src/app/pages/student/student.module.ts
+++ src/app/pages/student/student.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './student.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-
+const studentRoute: Routes = [
+  {
+    path: '', component: StudentComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent }
+    ]
+  }]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forChild(studentRoute),
+    CommonModule,
+    HttpClientModule,
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class StudentModule { }
