ce58e46ddbf69987ab61e71a9a9c3ce914344791
Salmanulfaris-M <fsalmanul13@gmail.com>
2023-06-06 14:00:03 +0530

    environment specify

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  backendUrl:"http://localhost:3001/",
+  firebase: {
+    apiKey: "AIzaSyBuDl_nSTpKOc6a_FzabCvQW2UtqnLuffE",
+    authDomain: "e-mail-otp-verification.firebaseapp.com",
+    projectId: "e-mail-otp-verification",
+    storageBucket: "e-mail-otp-verification.appspot.com",
+    messagingSenderId: "481187461752",
+    appId: "1:481187461752:web:f8255469cf48b74e5d0b8d",
+    measurementId: "G-DGKX0B9QDQ"
+  }
+};
