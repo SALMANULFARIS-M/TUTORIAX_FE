50f5c98a13f8ef5b71c54d1ca2fa9e53a11da7a8
Salmanulfaris-M <fsalmanul13@gmail.com>
2023-07-13 16:07:53 +0530

    back to top button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tutoriax_FE';
+
+  ngOnInit(): void {
+    var toTopButton = document.getElementById("to-top-button") as HTMLElement;
+
+    // When the user scrolls down 200px from the top of the document, show the button
+    window.onscroll = function () {
+      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        toTopButton.classList.remove("hidden");
+      } else {
+        toTopButton.classList.add("hidden");
+      }
+    }
+  }
+  goToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
 }
